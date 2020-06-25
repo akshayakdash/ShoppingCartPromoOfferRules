8d2c05665262c92002a452e916a70ef3f24fae47
Akshaya Kumar Dash <akshayakumardash@ymail.com>
2020-06-26 01:31:49 +0530

    Dtos added for Product and Cart model.

---
+++ ShoppingCart/Dtos/CartDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Dtos
+{
+    public class CartDto
+    {
+        public string CartId { get; set; }
+        public List<CartItemDto> CartItems { get; set; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                return CartItems.Sum(p => p.OfferPrice);
+            }
+        }
+    }
+
+    public class CartItemDto
+    {
+        public string SKU { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal OfferPrice { get; set; }
+        public decimal ActualPrice
+        {
+            get
+            {
+                return UnitPrice * Quantity;
+            }
+        }
+        public bool PromoApplied { get; set; }
+        public string PromoId { get; set; }
+    }
+}
---
+++ ShoppingCart/Dtos/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Dtos
+{
+    public class ProductDto
+    {
+        public string SKU { get; set; }
+        public decimal Price { get; set; }
+    }
+}
