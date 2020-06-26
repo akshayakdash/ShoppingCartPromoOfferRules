74ad799b6f64d0d64b78e85771bc43bf44d1cf1b
Akshaya Kumar Dash <akshayakumardash@ymail.com>
2020-06-26 08:07:45 +0530

    Tests added for calculating offer price. Offer price should be less than actual price and offer price can't be negative.

--- ShoppingCart.Tests/PromoRuleTests.cs
+++ ShoppingCart.Tests/PromoRuleTests.cs
@@ -67,5 +67,97 @@ namespace ShoppingCart.Tests
                 Assert.False(item.First().PromoApplied);
             }
         }
+
+        [Fact]
+        public void Valid_Promo_Should_Be_Applied_To_CartItem()
+        {
+            var promoRules = new List<PromoOffer> {
+                new PromoOffer
+                {
+                    PromotionOfferId = "1",
+                    PromotionOfferDescription = "3 A's for 130",
+                    ValidTill = DateTime.Today.AddDays(10),
+                    PromoRule = new PromoRule { IsForDifferentItems = false, Quantity = 3, SKUs = new List<string>{ "A" }, PromoResult = new PromoResult { OffFixedPrice = 130 } },
+                },
+            };
+
+            var cartItems = new List<Dtos.CartItemDto> {
+                    new CartItemDto { SKU = "A", Quantity = 3, UnitPrice = 50 },
+            };
+            var cartItemGroupedByPromo = _promoCalculator.ApplyPromoRule(cartItems, promoRules);
+            foreach (var item in cartItemGroupedByPromo)
+            {
+                // should not group items based on promoid
+                Assert.Equal(item.Key, promoRules.First().PromotionOfferId);
+                // should not apply any promo to cart item
+                Assert.True(item.First().PromoApplied);
+            }
+        }
+
+        [Fact]
+        public void Offer_Price_ShouldBe_Calculated_For_CartItem_With_Matching_PromoRules()
+        {
+            var promoRules = new List<PromoOffer> {
+                new PromoOffer
+                {
+                    PromotionOfferId = "1",
+                    PromotionOfferDescription = "3 A's for 130",
+                    ValidTill = DateTime.Today.AddDays(10),
+                    PromoRule = new PromoRule { IsForDifferentItems = false, Quantity = 3, SKUs = new List<string>{ "A" }, PromoResult = new PromoResult { OffFixedPrice = 130 } },
+                },
+            };
+
+            var cartItems = new List<Dtos.CartItemDto> {
+                    new CartItemDto { SKU = "A", Quantity = 3, UnitPrice = 50 },
+            };
+            var cartItemGroupedByPromo = _promoCalculator.ApplyPromoRule(cartItems, promoRules);
+            var calculatedCartItems = _promoCalculator.CalculateOfferPrice(cartItemGroupedByPromo, promoRules);
+
+            Assert.False(calculatedCartItems.First().OfferPrice < 0);
+        }
+
+        [Fact]
+        public void Offer_Price_For_Item_With_Valid_Promo_Should_Be_Less_Than_Actual_Price()
+        {
+            var promoRules = new List<PromoOffer> {
+                new PromoOffer
+                {
+                    PromotionOfferId = "1",
+                    PromotionOfferDescription = "3 A's for 130",
+                    ValidTill = DateTime.Today.AddDays(10),
+                    PromoRule = new PromoRule { IsForDifferentItems = false, Quantity = 3, SKUs = new List<string>{ "A" }, PromoResult = new PromoResult { OffFixedPrice = 130 } },
+                },
+            };
+
+            var cartItems = new List<Dtos.CartItemDto> {
+                    new CartItemDto { SKU = "A", Quantity = 3, UnitPrice = 50 },
+            };
+            var cartItemGroupedByPromo = _promoCalculator.ApplyPromoRule(cartItems, promoRules);
+            var calculatedCartItems = _promoCalculator.CalculateOfferPrice(cartItemGroupedByPromo, promoRules);
+
+            Assert.True(calculatedCartItems.First().OfferPrice <= cartItems.First().Quantity * cartItems.First().UnitPrice);
+        }
+
+        [Fact]
+        public void Offer_Price_For_Item_With_Valid_Promo_Should_Not_Be_In_Negative()
+        {
+            var promoRules = new List<PromoOffer> {
+                new PromoOffer
+                {
+                    PromotionOfferId = "1",
+                    PromotionOfferDescription = "3 A's for 130",
+                    ValidTill = DateTime.Today.AddDays(10),
+                    PromoRule = new PromoRule { IsForDifferentItems = false, Quantity = 3, SKUs = new List<string>{ "A" }, PromoResult = new PromoResult { OffFixedPrice = 130 } },
+                },
+            };
+
+            var cartItems = new List<Dtos.CartItemDto> {
+                    new CartItemDto { SKU = "A", Quantity = 3, UnitPrice = 50 },
+            };
+            var cartItemGroupedByPromo = _promoCalculator.ApplyPromoRule(cartItems, promoRules);
+            var calculatedCartItems = _promoCalculator.CalculateOfferPrice(cartItemGroupedByPromo, promoRules);
+
+            Assert.False(calculatedCartItems.First().OfferPrice < 0);
+        }
     }
 }
--- ShoppingCart/PromoOfferManager.cs
+++ ShoppingCart/PromoOfferManager.cs
@@ -24,7 +24,12 @@ namespace ShoppingCart
         {
 
         }
-
+        /// <summary>
+        /// Groups cart item based on promo rule applied
+        /// </summary>
+        /// <param name="orderItems"></param>
+        /// <param name="offers"></param>
+        /// <returns></returns>
         public IEnumerable<IGrouping<string, CartItemDto>> ApplyPromoRule(List<CartItemDto> orderItems, List<PromoOffer> offers)
         {
             var promoOffers = offers.Where(promo => promo.ValidTill >= DateTime.Now).ToList();
@@ -53,6 +58,93 @@ namespace ShoppingCart
                 }
             }
             return orderItems.GroupBy(p => p.PromoId).ToList();
+        }
+        
+        /// <summary>
+        /// Calculate offer price for items based on promo rule applied
+        /// </summary>
+        /// <param name="groupedCartItems"></param>
+        /// <param name="offers"></param>
+        /// <returns></returns>
+        public List<CartItemDto> CalculateOfferPrice(IEnumerable<IGrouping<string, CartItemDto>> groupedCartItems, List<PromoOffer> offers)
+        {
+            if (groupedCartItems != null && groupedCartItems.Any())
+            {
+                foreach (var promoGroup in groupedCartItems)
+                {
+                    var offer = offers.FirstOrDefault(p => p.PromotionOfferId == promoGroup.Key);
+                    if (offer != null)
+                    {
+                        // for same item
+                        if (promoGroup.Count() == 1)
+                        {
+                            var orderItem = promoGroup.First();
+                            if (orderItem.Quantity >= offer.PromoRule.Quantity)
+                            {
+                                var promoFrequency = orderItem.Quantity / offer.PromoRule.Quantity;
+                                var promoNotMatchingItemCount = orderItem.Quantity % offer.PromoRule.Quantity;
+                                decimal itemPrice = default(decimal);
+                                if (offer.PromoRule.PromoResult.OffFixedPrice > 0)
+                                {
+                                    // offer price for item counts
+                                    itemPrice = promoFrequency * offer.PromoRule.PromoResult.OffFixedPrice;
+                                }
+                                else if (offer.PromoRule.PromoResult.OffPercentage > 0)
+                                {
+                                    itemPrice = (offer.PromoRule.Quantity * orderItem.UnitPrice) * Convert.ToDecimal(offer.PromoRule.PromoResult.OffPercentage / 100);
+                                }
+                                // unit price for item not matching counts
+                                if (promoNotMatchingItemCount > 0)
+                                    itemPrice = itemPrice + Convert.ToDecimal(promoNotMatchingItemCount * orderItem.UnitPrice);
+
+                                orderItem.OfferPrice = itemPrice;
+                            }
+                        }
+                        else if (promoGroup.Count() > 1)
+                        {
+                            // for different items
+                            var items = promoGroup.ToList();
+                            var lastItem = items.Last();
+                            items.ForEach(item =>
+                            {
+                                if (item.SKU == lastItem.SKU)
+                                {
+                                    item.OfferPrice = offer.PromoRule.PromoResult.OffFixedPrice;
+                                    // apply price for remainder item
+                                    if (item.Quantity > offer.PromoRule.Quantity)
+                                    {
+                                        var promoNotMatchingItemCount = item.Quantity - offer.PromoRule.Quantity;
+                                        if (promoNotMatchingItemCount > 0)
+                                            item.OfferPrice += Convert.ToDecimal(promoNotMatchingItemCount * item.UnitPrice);
+                                    }
+                                }
+                                else
+                                {
+                                    item.OfferPrice = 0;
+                                    // apply price for remainder item
+                                    if (item.Quantity > offer.PromoRule.Quantity)
+                                    {
+                                        var promoNotMatchingItemCount = item.Quantity - offer.PromoRule.Quantity;
+                                        if (promoNotMatchingItemCount > 0)
+                                            item.OfferPrice += Convert.ToDecimal(promoNotMatchingItemCount * item.UnitPrice);
+                                    }
+                                }
+                            });
+                        }
+                    }
+                    else
+                    {
+                        promoGroup.ToList().ForEach(item =>
+                        {
+                            item.OfferPrice = item.UnitPrice * item.Quantity;
+                        });
+                    }
+                }
+            }
+            var groupedItems = groupedCartItems.SelectMany(p => p.ToList()).ToList();
+
+            // check if any cart item offer price is greater than the actual price
+            return groupedItems;
         }
     }
 }
